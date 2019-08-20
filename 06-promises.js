edad971a783006ee16249c9b5b1497641e03751c
Eltech-git <emilianolombarditech@gmail.com>
2019-08-20 18:52:41 +0700

    Promises kata finish

--- 06-promises.js
+++ 06-promises.js
@@ -62,3 +62,58 @@ getUp().then(res => {console.log(res);
 // haveBreakfast().then(response => {console.log(response);})
 // getUp().then(response => {console.log(response)})
 // brushTeeth().then(response => {console.log(response)})
+
+
+
+//020602
+
+const makeCoffee = () => {
+	return new Promise ((res,rej)=> {
+		setTimeout(() => {
+			res('coffee')
+		},10000)
+	})
+}
+
+const makeEggs = () => {
+	return new Promise ((res,rej)=> {
+		setTimeout(() => {
+			res('eggs')
+		},5000)
+	})
+}
+const makeToast = () => {
+	return new Promise ((res,rej)=> {
+		setTimeout(() => {
+			res('toast')
+		},6000)
+	})
+}
+const makeBacon = () => {
+	return new Promise ((res,rej)=> {
+		setTimeout(() => {
+			res('bacon')
+		},8000)
+	})
+}
+makeBacon().then(res => {console.log(res);})
+makeToast().then(res => {console.log(res);})
+makeEggs().then(res => {console.log(res);})
+makeCoffee().then(res => {console.log(res);})
+Promise.all([makeEggs(),makeBacon(),makeToast(),makeCoffee()]).then(res=> console.log(res))
+
+//020603
+
+let speedy = new Promise((res, rej) => {
+	let num = Math.random()*4
+  return setTimeout(() => res('Speedy is the winner'), num)
+})
+let jackpot = new Promise((res, rej) => {
+	let num = Math.random()*4
+  return setTimeout(() => res('Jackpot is the winner'), num)
+})
+let red = new Promise((res, rej) => {
+	let num = Math.random()*4
+  setTimeout(() => res('Red is the winner'), num)
+})
+Promise.race([speedy,jackpot,red]).then(response => console.log(response))
