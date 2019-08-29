725b4650636cb8f55cf4cef0a1946f266a60cfd0
Eltech-git <emilianolombarditech@gmail.com>
2019-08-29 16:16:24 +0700

    060303

---
+++ React/060101/src/components/Favorites.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/favorites.css'
+
+class Favorites extends React.Component {
+	state = {}
+	render() {
+		return (
+			<div className = 'favorites'>
+				FUNZIONA DIO
+				SALAMANDRA
+			</div>
+
+		)
+	}
+}
+export default Favorites;
