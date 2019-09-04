65491fadf875858ac0c6f791ad6cc8d5db799a7d
Eltech-git <emilianolombarditech@gmail.com>
2019-09-04 12:49:52 +0700

    070103

---
+++ Node/0701-Modules/calc.js
@@ -0,0 +1,12 @@
+let add = array => {
+  return array.reduce((a, b) => a + b);
+};
+let subtract = array => {
+  return array.reduce((a, b) => a + b);
+};
+
+module.exports = {
+  add: add,
+  substract: substract
+};
+console.log(add(pippo));
